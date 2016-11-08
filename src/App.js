a0c283c2289ff673bcf1608f2e6932065397110f
Trent Lennon <tlennon@EBSCO.COM>
2016-11-08 14:23:41 -0500

    Everything seems to be working as a rough proof of concept.

--- src/App.js
+++ src/App.js
@@ -2,57 +2,159 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  changeContent = (e) => {
-    this.setState({inputContent: e.target.value})
-  } 
-
-  constructor() {
-    super();
+ 
+  constructor(props) {
+    super(props);
     
     this.state = {
-      responseObjectName: ""
+      responseObjectName: "",
+      pastedText: "",
+      builtCode: "",
+      errorMessage: "",
+      createTokenMethod: ""
     };
 
-    this.changeResponseObjectName = this.changeResponseObjectName.bind(this);
-    this.changeResponseName = this.changeResponseName.bind(this);
     this.changePastedText = this.changePastedText.bind(this);
-  }
-
-  changeResponseObjectName(event) {
-    this.setState({responseObjectName : event.target.value});
-
-    console.log(this.state);
-  }
-
-  changeResponseName(event) {
-    this.setState({responseName : event.target.value});
-    console.log(this.state);
+    this.changeErrorMessage = this.changeErrorMessage.bind(this);
+    this.changeCreateTokenMethod = this.changeCreateTokenMethod.bind(this);
     
+    this.returnBuilderObject = this.returnBuilderObject.bind(this);
+    this.createBuilder = this.createBuilder.bind(this);
+    
+  }
+
+  returnBuilderObject(parentBuilderName, paramName, paramType) {
+    return `
+        //TODO: All methods in a builder must have an explanation of the parameters being added.
+        public ${parentBuilderName} Add${paramName}(${paramType} parameterValue)
+        {
+            RequestObject.${paramName} = parameterValue;
+            return this;
+        }
+    `
+  }
+  
+  changeCreateTokenMethod(event) {
+    this.setState({createTokenMethod : event.target.value});
+  }
+
+  changeErrorMessage(event) {
+    this.setState({errorMessage : event.target.value});
   }
 
   changePastedText(event) {
     this.setState({pastedText : event.target.value});
-    console.log(this.state);
-    
   }
+
+  createBuilder() {
+    //pastedText gets passed to the 'parser'
+    var splitValues = this.state.pastedText.split('\n');
+    var relevantData = [];
+    var className = "";
+    var parentBuilderName = "";
+
+    splitValues.forEach((element) => {
+      if(element.includes("public class")) {
+        className = element.trim().split(' ')[2];
+        parentBuilderName = `${className}Builder`;
+      }
+
+      else if(element.includes("public") && element.includes("{")) {
+        relevantData.push(element.trim());
+      }
+    });
+
+    var builtCodeBody = "";
+    //Rebuild relevantData with relevant builder text.
+    relevantData.forEach((element, index) => {
+      var lexicons = element.split(' ');
+      var type = lexicons[1];
+      var name = lexicons[2];
+
+      builtCodeBody += this.returnBuilderObject(parentBuilderName, name, type) + "\n";
+    });
+
+
+//TODO: Need to add a component for the MeerkatAPIRequestBase
+    var builtCodeHeader = `
+    using System;
+    using EP.PAL.BASE;
+    using EP.PAL.BASE.WebService.INF;
+    namespace //TODO: Namespace must be set.
+    {
+      //TODO: All classes require an explanation of the purpose of the request builder.
+      public class ${parentBuilderName} : MeerkatAPIRequestBase<${parentBuilderName}>
+      {
+
+        // Fields to hold data
+        private const string CreateTokenMethod = "${this.createTokenMethod}";
+        internal ${this.state.requestObjName} RequestObject = new ${this.state.requestObjName}();
+
+        /// <summary>
+        /// constructor to the class
+        /// </summary>
+        /// <param name="ServiceURL">ServiceURL</param>
+        /// <param name="Options">MessageOptions</param>
+        /// <param name="Logger">IPALLogging</param>
+        internal ${parentBuilderName}(string ServiceURL, MessageOptions Options, IPALLogging Logger)
+            : base(Options, Logger)
+        {
+            EndPoint = URLHelper.CombineUri(ServiceURL, CreateTokenMethod);
+        }
+
+    `
+
+    var builtCodeFooter = `
+        /// <summary>
+        /// Send the request to Meerkat         
+        /// </summary>
+        /// <returns>Returns an APIResponse message with the APIResponse inside it.</returns>
+        public APIResponse<${className}Response> Send()
+        {
+            LogObj.WriteInfo("Performing Request");
+
+            return RestClient.SendRequestSerializeDeserialize<${className}Response, ${this.errorMessage}>(this, true);
+        }
+
+        /// <summary>
+        /// Request Data Object
+        /// </summary>
+        protected override object RequestDataObject
+        {
+            get { return RequestObject; }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
+
+`
+    var compiledCode = builtCodeHeader + builtCodeBody + builtCodeFooter;
+     this.setState({ builtCode: compiledCode});
+
+  }
+
 
   render() {
     return (
-      <div className='builder'>
+      <div className='builder'>     
         <div className='fieldContainer'>
-          <div className='label'>Response Object Name:</div>
+          <div className='label'>Error Message Name:</div>
             <input
               type='textbox'
-              value={this.state.responseObjName}
-              onChange={this.changeResponseObjectName}
+              value={this.state.errorMessage}
+              onChange={this.changeErrorMessage}
               />
         </div>
         <div className='fieldContainer'>
-          <div className='label'>Response Name:</div>
+          <div className='label'>Create Token Method:</div>
             <input
               type='textbox'
-              value={this.state.responseName}
-              onChange={this.changeResponseName}
+              value={this.state.createTokenMethod}
+              onChange={this.changeCreateTokenMethod}
               />
         </div>
         <textarea
@@ -64,10 +166,18 @@ class App extends Component {
         </textarea>
         <input
           className='fieldContainer'
-          onChange={this.handleChange}
+          onClick={this.createBuilder}
           type='button'
           value='Generate builder class'
           />
+
+          <div>
+            <pre>
+              <code className="language-csharp">
+                {this.state.builtCode}
+              </code>
+            </pre>
+          </div>
       </div>
     );
   }
